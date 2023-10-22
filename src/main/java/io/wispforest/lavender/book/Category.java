e0b7b2a073d805d46a8e199a807356e5584a6193
glisco <glisco03@web.de>
2023-10-22 02:21:37 +0200

    implement support for sorting entries and categories by ordinal

--- src/main/java/io/wispforest/lavender/book/Category.java
+++ src/main/java/io/wispforest/lavender/book/Category.java
@@ -3,4 +3,11 @@ package io.wispforest.lavender.book;
 import net.minecraft.item.Item;
 import net.minecraft.util.Identifier;
 
-public record Category(Identifier id, String title, Item icon, boolean secret, String content) implements Book.BookmarkableElement {}
+public record Category(
+        Identifier id,
+        String title,
+        Item icon,
+        boolean secret,
+        int ordinal,
+        String content
+) implements Book.BookmarkableElement {}
